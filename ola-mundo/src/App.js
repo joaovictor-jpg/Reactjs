0cc0ce59fad9b4f20df9fda17cda269b0397f8f2
João Victor <joaovictormdasilva676@gmail.com>
2024-08-16 20:17:37 -0300

    Rotas

--- ola-mundo/src/App.js
+++ ola-mundo/src/App.js
@@ -1,11 +1,16 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Inicio from "./paginas/Inicio";
 import SobreMin from "./paginas/SobreMin";
 
-const pagina = window.location.pathname === '/' ? <Inicio /> : <SobreMin />
-
 function App() {
   return (
-    pagina
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Inicio />} />
+        <Route path="/sobremin" element={<SobreMin />} />
+        <Route path="*" element={<div>Página não encontrada</div>} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
