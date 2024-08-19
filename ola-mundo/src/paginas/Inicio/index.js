5d54835b11cd4856f03bae1946939cbfd17f8f4d
João Victor <joaovictormdasilva676@gmail.com>
2024-08-18 21:55:45 -0300

    Criando rotas aninhadas

--- ola-mundo/src/paginas/Inicio/index.js
+++ ola-mundo/src/paginas/Inicio/index.js
@@ -5,15 +5,12 @@ import Post from "componentes/Post";
 
 export default function Inicio() {
     return (
-        <main>
-            <Banner />
-            <ul className={style.posts}>
-                {posts.map((post) => (
-                    <li key={post.id}>
-                        <Post post={post} />
-                    </li>
-                ))}
-            </ul>
-        </main>
+        <ul className={style.posts}>
+            {posts.map((post) => (
+                <li key={post.id}>
+                    <Post post={post} />
+                </li>
+            ))}
+        </ul>
     );
 };
--- ola-mundo/src/route.js
+++ ola-mundo/src/route.js
@@ -3,14 +3,17 @@ import Inicio from "./paginas/Inicio";
 import SobreMin from "./paginas/SobreMin";
 import Menu from "./componentes/Menu";
 import Rodape from "componentes/Rodape";
+import PaginaPadrao from "componentes/PaginaPadrao";
 
 function AppRoute() {
   return (
     <BrowserRouter>
       <Menu />
       <Routes>
-        <Route path="/" element={<Inicio />} />
-        <Route path="/sobremim" element={<SobreMin />} />
+        <Route path="/" element={<PaginaPadrao />}>
+          <Route path="/" element={<Inicio />} />
+          <Route path="/sobremim" element={<SobreMin />} />
+        </Route>
         <Route path="*" element={<div>Página não encontrada</div>} />
       </Routes>
       <Rodape />
